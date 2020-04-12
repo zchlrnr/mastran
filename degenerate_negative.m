c1b709db361a72111550c02e05c58c0e8ebbfe7d
zchlrnr <zchlrnr@gmail.com>
2020-04-11 20:07:13 -0700

    Updated degenerate_negatives.m with comments.

--- degenerate_negative.m
+++ degenerate_negative.m
@@ -1,13 +1,38 @@
 function fixed_number = degenerate_negative(char_in)
-    char_in = deblank(char_in);
+    %
+    %------------------------------------------------------------------%
+    %                     Theory Of Operations                         %
+    %------------------------------------------------------------------%
+    % Take in character array that must be a number
+    % Strips away all whitespace from start and end with strtrim
+    % replaces all '-' characters with 'E' if they aren't in position 1
+    % ditto for '+' characters.
+    % will break if raised to a power of ten in the hundreds place
+
+    char_in = strtrim(char_in);
     L = length(char_in);
-    if L > 1 && strncmp(char_in(L-1),'-',1)==1
+
+
+    % Repairing negatives
+    if L > 1 && strcmp(char_in(L-1),'-',1)==1
         fixed_number = str2double(strcat(num2str(...
             char_in(1:(L-2))),'E',num2str(char_in((L-1):L))));
-    elseif L > 2 && strncmp(char_in(L-2),'-',1)==1
+    elseif L > 2 && strcmp(char_in(L-2),'-',1)==1
         fixed_number = str2double(strcat(num2str(...
             char_in(1:(L-3))),'E',num2str(char_in((L-2):L))));
     else
         fixed_number = str2double(char_in);
     end
+
+    % Repairing positives
+    if L > 1 && strcmp(char_in(L-1),'+',1)==1
+        fixed_number = str2double(strcat(num2str(...
+            char_in(1:(L-2))),'E',num2str(char_in((L-1):L))));
+    elseif L > 2 && strcmp(char_in(L-2),'+',1)==1
+        fixed_number = str2double(strcat(num2str(...
+            char_in(1:(L-3))),'E',num2str(char_in((L-2):L))));
+    else
+        fixed_number = str2double(char_in);
+    end
+
 end
